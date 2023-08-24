af3138b0a3be2463f4fe3b66f5e6485e8aa7d6a5
davc1980 <davc1980@gmail.com>
2023-08-24 16:13:13 -0700

    adding tablas model

--- miweb/admin.py
+++ miweb/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tabla
 
 # Register your models here.
+
+admin.site.register(Tabla)
--- miweb/models.py
+++ miweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Tabla(models.Model):
+    nombre = models.CharField(max_length=30)
+    genero = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return self.nombre + '-->'+self.genero
